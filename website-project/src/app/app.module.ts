ce034cfd34cc44747db9b26bc52774c781b9e45d
KymberleeS <sableskymberlee@yahoo.com>
2021-04-29 15:20:01 -0400

    added home component

--- website-project/src/app/app.module.ts
+++ website-project/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
     NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
